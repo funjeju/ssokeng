fd1e5df8837c24c719fb3fe679b708c679fdb123
unknown <naggu1999@naver.com>
2026-05-02 18:44:07 +0900

    오류 수정

--- app/api/pdf/[sessionId]/route.ts
+++ app/api/pdf/[sessionId]/route.ts
@@ -21,7 +21,7 @@ export async function GET(
 
     const [buffer] = await file.download()
 
-    return new NextResponse(buffer, {
+    return new NextResponse(new Uint8Array(buffer), {
       headers: {
         'Content-Type': 'application/pdf',
         'Content-Disposition': 'inline',
